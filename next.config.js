22b9d1b3696e074d55e88928b8fa1293baa1c660
LeeCh0129 <cksgh5477@gmail.com>
2024-06-23 10:55:28 +0900

    CommonJS 모듈을 ES6 모듈로 불러오는 오류

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+// module.exports = {
+//   images: {
+//     domains: [
+//       "www.notion.so",
+//       "lh5.googleusercontent.com",
+//       "lh3.googleusercontent.com",
+//       "s3-us-west-2.amazonaws.com",
+//     ],
+//   },
+// }
 module.exports = {
   images: {
     domains: [
@@ -6,5 +16,13 @@ module.exports = {
       "lh3.googleusercontent.com",
       "s3-us-west-2.amazonaws.com",
     ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    })
+    return config
   },
 }
